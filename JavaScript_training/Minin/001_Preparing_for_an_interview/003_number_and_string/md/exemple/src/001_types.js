aeace155040f985134a75e4edef6b5400832eaf9
Konstantine899 <kostay375298918971@gmail.ru>
2021-04-25 22:12:53 +0300

    data type 004

---
+++ JavaScript_training/Minin/001_Preparing_for_an_interview/003_number_and_string/md/exemple/src/001_types.js
@@ -0,0 +1,15 @@
+// 001_types.js
+
+//Строки и числа.
+
+console.log(1 + '2'); // string 12
+console.log('' + 1 + 0); // string 10
+
+console.log('' - 1 + 0); // string 10
+console.log('3' * '8'); // number 24
+console.log(4 + 10 + 'px'); // 14px
+console.log('px' + 5 + 10); // px4510
+console.log('42' - 40); // number 2
+console.log('42px' - 2); // number 2
+console.log(null + 2); // number 2
+console.log(undefined + 42); // NaN
