476e15343176860221a400e10760027b14dbd1ab
Konstantine899 <kostay375298918971@gmail.ru>
2021-04-25 16:10:57 +0300

    data type 002

---
+++ JavaScript_training/Minin/001_Preparing_for_an_interview/002_type_conversion/src/001_types.js
@@ -0,0 +1,45 @@
+// 001_types.js
+
+// 001_types.js
+
+// null, undefined, boolean, number, string, object,  symbol
+
+// console.log(typeof 0);
+// console.log(typeof true);
+// console.log(typeof 'JavaScript'); // "", '', `` последняя называется литералами или шаблонная строка
+// console.log(typeof undefined);
+// console.log(typeof {});
+
+//или же в место {} могу передавать какие-то существующие классы Data, Math
+
+// console.log(typeof Math);
+// console.log(typeof Symbol('JS'));
+// console.log(typeof null);
+
+// console.log(typeof function () {});
+// console.log(typeof NaN);
+// console.log(typeof 1 / 0);
+
+// Приведение типов.
+
+// let language = 'JavaScript';
+
+// if (language) {
+//   console.log('The best language is:', language);
+// }
+
+// '', null, undefined, NaN, false
+
+// console.log(Boolean(''));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean(false));
+
+// console.log(Boolean('Hello'));
+// console.log(Boolean(' '));
+// console.log(Boolean('0'));
+// console.log(Boolean(0));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(function () {}));
