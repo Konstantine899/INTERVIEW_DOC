153bae0a1341877ad52fa4165996bcd26397eba1
Konstantine899 <kostay375298918971@gmail.ru>
2021-05-03 14:42:24 +0300

    context

---
+++ JavaScript_training/Minin/001_Preparing_for_an_interview/011_context/md/exemple/src/001_types.js
@@ -0,0 +1,24 @@
+// 001_types.js
+
+// == vs ===
+
+// console.log(2 == '2'); // true
+// console.log(2 === '2'); // false
+
+// console.log(undefined == null); // true
+// console.log(undefined === null); // false
+
+// console.log('0' == false); // true
+
+//========
+
+// console.log(false == ''); // true
+// console.log(false == []); // true
+// console.log(false == {}); // false
+// console.log('' == 0); //
+// console.log('' == []); //
+// console.log('' == {}); //
+
+console.log(0 == []); //
+console.log(0 == {}); //
+console.log(0 == null); //
