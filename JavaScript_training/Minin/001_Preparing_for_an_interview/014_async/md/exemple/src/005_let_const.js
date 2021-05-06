460c65155bd9aa221caa5e8f9a59577d6be5934d
Konstantine899 <kostay375298918971@gmail.ru>
2021-05-06 12:56:00 +0300

    async

---
+++ JavaScript_training/Minin/001_Preparing_for_an_interview/014_async/md/exemple/src/005_let_const.js
@@ -0,0 +1,35 @@
+// let
+
+// let a = 'Variable a';
+// let b = 'Variable b';
+
+// {
+//   a = 'New Variable A';
+//   let b = 'Local Variable B';
+//   console.log('Scope A', a);
+//   console.log('Scope B', b);
+// }
+
+// console.log(' A', a);
+// console.log(' B', b);
+
+///////////////////////////////
+
+// Const
+
+const PORT = 8080;
+const array = ['Javascript', 'is', 'Awesome'];
+array.push('!');
+array[0] = 'JS';
+
+console.log(array);
+
+const obj = {};
+obj.name = 'Konstantine';
+obj.age = 32;
+
+console.log(obj);
+
+delete obj.name;
+
+console.log(obj);
---
+++ JavaScript_training/Minin/001_Preparing_for_an_interview/014_async/md/exemple/src/011_async.js
@@ -0,0 +1,9 @@
+// async
+
+const first = () => console.log('First');
+const second = () => console.log('Second');
+const third = () => console.log('Third');
+
+first();
+setTimeout(second, 0);
+third();
